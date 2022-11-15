9cccfe7c4bb128d7c97ced0d08aab0fcfc3a06ee
BobbyDeluna <bobby.deluna2@gmail.com>
2022-11-14 23:03:58 -0700

    Completed Assignment

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,15 @@
-class Student 
+class Student < User
+
+    def initialize
+        @knowledge = []
+    end
+
+    def learn(arg)
+        self.knowledge << arg
+    end
+
+    def knowledge
+        @knowledge
+    end
 
 end
